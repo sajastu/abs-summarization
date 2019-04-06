81d94a48c3610ca6f9fad36513979cdcfe85a442
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2019-04-06 11:52:26 -0400

    adding tracer

--- onmt/encoders/star_transformer.py
+++ onmt/encoders/star_transformer.py
@@ -104,8 +104,9 @@ class StarTransformerEncoder(EncoderBase):
             embs = embs + P
 
         nodes = embs  # nodes variable denotes the hidden states of source input
-        # import pdb;pdb.set_trace()
         relay = embs.mean(2, keepdim=True)
+        import pdb;pdb.set_trace()
+
         ex_mask = smask[:, None, :, None].expand(B, H, L, 1)
 
         r_embs = embs.view(B, H, 1, L)
